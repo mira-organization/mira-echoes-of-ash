04a05227dc66def456270cff18406a8705d6726b
exepta <genshin.exepta@outlook.com>
2025-06-13 02:05:21 +0200

    feat(MEO-16): fix coverage

--- crates/game_load/src/lib.rs
+++ crates/game_load/src/lib.rs
@@ -1,7 +1,10 @@
+#![feature(coverage_attribute)]
+
 use bevy::prelude::*;
 
 pub struct GameLoadPlugin;
 
 impl Plugin for GameLoadPlugin {
+    #[coverage(off)]
     fn build(&self, _app: &mut App) {}
 }
